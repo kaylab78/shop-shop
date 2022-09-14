5fb6f8193e6da979716dc55f0e7b5f2bc9cc7162
kaylab78 <kaylabackus78@gmail.com>
2022-09-14 14:30:12 -0500

    Implement IndexedDB functionality.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -15,6 +15,8 @@ import {
 
 import Cart from "../components/Cart";
 
+import { idbPromise } from "../utils/helpers";
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
@@ -50,15 +52,31 @@ function Detail() {
   };
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
+      setCurrentProduct(products.find(product => product._id === id));
+    } 
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products,
+        products: data.products
+      });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
       });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
